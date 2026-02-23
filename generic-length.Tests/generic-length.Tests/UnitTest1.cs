7033a42261fc63dac89090a8accc41b4dd830097
Vaibhavi01 <varshneyvaibhavi65@gmail.com>
2026-02-23 12:14:01 +0530

    [Vaibhavi].Add- Implemented UC2- Inch Equality

---
+++ generic-length.Tests/generic-length.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace generic_length.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
