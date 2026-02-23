0dde5dc925127123c43493be48fdab0a066662a4
Vaibhavi01 <varshneyvaibhavi65@gmail.com>
2026-02-23 12:26:06 +0530

    [Vaibhavi].Add- Implemented UC3- Generic Length

--- feet-measurement-equality/InvalidFeetException.cs
+++
@@ -1,8 +0,0 @@
-using System;
-namespace feet_measurement_equality{
-    public class InvalidFeetException : Exception {
-        public InvalidFeetException(string message) : base(message){
-            
-        }
-    }
-}
